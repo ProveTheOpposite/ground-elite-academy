28510ebf4500d09657ce67d6623bf61a1b24e1ca
alex <alex@gmail.com>
2024-10-17 12:16:57 +0200

    Change env variables of emailJS

--- src/pages/ContactUs/index.jsx
+++ src/pages/ContactUs/index.jsx
@@ -90,8 +90,8 @@ const ContactUs = () => {
     setIsLoading(true);
     try {
       await emailjs.send(
-        import.meta.env.VITE_SERVICE_ID,
-        import.meta.env.VITE_TEMPLATE_ID,
+        import.meta.env.VITE_EMAILJS_SERVICE_ID,
+        import.meta.env.VITE_EMAILJS_TEMPLATE_ID,
         values,
         import.meta.env.VITE_EMAILJS_PUBLIC_KEY,
       );
