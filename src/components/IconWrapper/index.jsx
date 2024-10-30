7a2bfcb277f551cd98d7e9dfb9ba253ce849e535
alex <alex@gmail.com>
2024-10-30 12:06:19 +0100

    Fix margin right to icon for Gaetan Houara

--- src/components/IconWrapper/index.jsx
+++ src/components/IconWrapper/index.jsx
@@ -3,7 +3,7 @@ import PropTypes from "prop-types";
 
 const IconWrapper = ({ align, icon }) => (
   <div
-    className={`${align === "right" ? "min-[1920px]:float-right min-[1920px]:ml-3" : ""} float-left mr-3 flex items-center justify-center rounded-[10px] bg-red-600 bg-opacity-10 xl:mt-1.5`}
+    className={`${align === "right" ? "min-[1920px]:float-right min-[1920px]:ml-3 min-[1920px]:mr-0" : ""} float-left mr-3 flex items-center justify-center rounded-[10px] bg-red-600 bg-opacity-10 xl:mt-1.5`}
     style={{ width: "40px", height: "40px" }}
   >
     <span className="text-sm md:text-base">
